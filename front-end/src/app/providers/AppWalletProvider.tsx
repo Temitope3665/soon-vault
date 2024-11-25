d7456b09306ed4908202f79c5ef0a4791d75ac43
Your Other Name <your-email-account2@example.com>
2024-11-26 00:28:54 +0100

    update network

--- front-end/src/app/providers/AppWalletProvider.tsx
+++ front-end/src/app/providers/AppWalletProvider.tsx
@@ -12,10 +12,10 @@ require('@solana/wallet-adapter-react-ui/styles.css');
 
 export default function AppWalletProvider({ children }: { children: React.ReactNode }) {
   const network = WalletAdapterNetwork.Devnet;
-  // const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
   const endpoint = useMemo(() => {
     if (network === WalletAdapterNetwork.Devnet) {
-      return 'https://api.devnet.solana.com';
+      return 'https://rpc.devnet.soo.network/rpc';
     }
 
     return clusterApiUrl(network);
@@ -25,13 +25,6 @@ export default function AppWalletProvider({ children }: { children: React.ReactN
     () => [new NightlyWalletAdapter(), new PhantomWalletAdapter(), new SolflareWalletAdapter({ network }), new TorusWalletAdapter()],
     [network]
   );
-  // const wallets = useMemo(
-  //   () => [
-  //     // manually add any legacy wallet adapters here
-  //     // new UnsafeBurnerWalletAdapter(),
-  //   ],
-  //   []
-  // );
 
   return (
     <ConnectionProvider endpoint={endpoint}>
